31fbf01cd21cd1f1fc642f6e061442c15fb7f16e
Zach Ayers <ayers.dev@icloud.com>
2020-08-15 16:05:48 -0500

    init

--- main.go
+++ main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
-type link string
-
 func main() {
-	links := []link{
+	links := []string{
 		"http://www.google.com",
 		"http://www.facebook.com",
 		"http://www.stackoverflow.com",
@@ -16,21 +15,31 @@ func main() {
 		"http://www.golang.org",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		go link.checkStatus()
+		go checkStatus(link, c)
 	}
 
+	for l := range c {
+		link := l
+		go func() {
+			time.Sleep(5 * time.Second)
+			checkStatus(link, c)
+		}()
+	}
 
 }
 
-func (l link) checkStatus() {
-	_, err := http.Get(string(l))
+func checkStatus(l string, c chan string) {
+	_, err := http.Get(l)
 
 	if err != nil {
 		fmt.Println("Down: ", l)
+		c <- l
 		return
 	}
 
 	fmt.Println("Up: ", l)
-
+	c <- l
 }
